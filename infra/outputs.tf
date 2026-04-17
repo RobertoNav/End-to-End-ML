65cbe14c291e57d20db3a0109b82c9a0fcb27871
Tiz . <tirzah03pb@gmail.com>
2026-04-17 16:22:34 -0600

    fix: validate state

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,14 @@
 output "ec2_public_ip" {
-  description = "IP pública de la instancia EC2 requerida por la Persona 4"
+  description = "IP publica de la instancia EC2 requerida por la Persona 4"
   value       = aws_instance.mlops_server.public_ip
 }
 
+output "ec2_instance_id" {
+  description = "ID de la instancia EC2 desplegada"
+  value       = aws_instance.mlops_server.id
+}
+
 output "s3_bucket_name" {
-  description = "Nombre del bucket S3 de la aplicación"
+  description = "Nombre del bucket S3 de la aplicacion"
   value       = aws_s3_bucket.app_bucket.id
 }
